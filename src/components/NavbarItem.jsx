7f96e348366f3547da12c33db8d1867438b28a47
Utkarsh Katiyar <utkarsh_katiyar@outlook.com>
2023-02-25 12:36:39 +0530

    Components Added

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+
+import { useSearchParams } from "next/navigation";
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  return (
+    <div>
+      <Link
+        className={`m-4 hover:text-amber-700 font-semibold p-2 ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-4  decoration-amber-700 rounded-lg"
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
