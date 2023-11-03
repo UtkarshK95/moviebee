6e71caaaac34d6908be17873b94b36fcb14b906c
Utkarsh Katiyar <utkarsh_katiyar@outlook.com>
2023-11-03 23:43:18 +0530

    Updated Dark/Light Mode. Updated Card Styles.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group p-8">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
@@ -14,9 +14,7 @@ export default function Card({ result }) {
           height={300}
           className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
           style={{
-            maxWidth: "100%",
-            height: "auto",
-            width: "auto",
+            height: "300px",
           }}
           placeholder="blur"
           blurDataURL="/spinner.svg"
